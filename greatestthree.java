1f4c1beac0168740a3de8e5a5f749e1f06357122
DeependraRao01 <87476279+DeependraRao01@users.noreply.github.com>
2021-11-27 19:58:17 +0530

    Add files via upload

---
+++ greatestthree.java
@@ -0,0 +1,31 @@
+package deependra;
+import java.util.*;
+
+public class greatestthree {
+
+
+		public static void main(String[] args) {
+			
+			int num1, num2, num3;
+			Scanner sc = new Scanner(System.in);
+			
+			num1 = sc.nextInt();
+			num2 = sc.nextInt();
+			num3 = sc.nextInt();
+			
+			if(num1 > num2 && num1 > num3)
+			{
+				System.out.println(num1);
+			}
+			else if(num2 > num3) 
+			{
+				System.out.println(num2);
+			}
+			else
+			{
+				System.out.println(num3);
+			}
+
+		}
+
+	}
